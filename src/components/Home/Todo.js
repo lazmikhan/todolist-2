1353ed1e81760c15d09e81bb7ebddfeca642c39e
Toya Lazmi Khan <86733350+lazmikhan@users.noreply.github.com>
2023-06-15 07:27:17 +0800

    updat

--- src/components/Home/Todo.js
+++ src/components/Home/Todo.js
@@ -6,9 +6,16 @@ import { app, db } from '../Authentication/config/config';
 const Todo = (props) => {
   const [todos, setTodos] = useState([]);
   const [todoArray, setTodoArray] = useState([]);
+  const [time, setTime] = useState(null);
+const [currT, setCurrT]= useState(new Date());
 
+ 
   const handleInputChange = (e) => {
     setTodos(e.target.value);
+  };
+  const handleTimeChange = (e) => {
+    setTime(e.target.value);
+    console.log(time);
   };
   const getAllDocuments = async () => {
     const todosCollectionRef = collection(db, `todos of ${props.userUID}`);
@@ -27,22 +34,48 @@ const Todo = (props) => {
   
    
     await setDoc(newDocRef, {
-      email: props.currentUser,
+      time: time,
       todos: todos,
     });
 
   };
   useEffect(()=>{
+    const timeoutIds = [];
    const getData=async()=>{
     console.log(await getAllDocuments());
     const todoArr = await getAllDocuments();
     setTodoArray(todoArr);
    }
     getData();
+     todoArray.forEach(todo => {
+    const [hours, minutes] = todo.time.split(':');
+    const reminderTime = new Date();
+    reminderTime.setHours(hours);
+    reminderTime.setMinutes(minutes);
+
+    const currentTime = new Date();
+    const timeDifference = reminderTime - currentTime;
+console.log(timeDifference);
+    if (timeDifference > 0) {
+      const timeoutId = setTimeout(() => {
+        alert(`Reminder: ${todo.todos}`);
+        const result =alert(`Reminder: ${todo.todos}`);
+        if (result) {
+            clearTimeout(timeoutId);
+          }
+      }, timeDifference);
+      timeoutIds.push(timeoutId);
+      return () => {
+       
+      };
+    }
+  });
     return () => {
         // Cleanup logic
       };
+
   },[todoArray])
+ 
   const handleDeleteTodo = async(todoId) => {
     const todoRef = doc(db, `todos of ${props.userUID}`, todoId);
 
@@ -58,9 +91,10 @@ const Todo = (props) => {
   const handleUpdateTodo = async(todoId) => {
     const todoRef = doc(db, `todos of ${props.userUID}`, todoId);
 const newTodo=prompt("Edit Your Todo");
+const newTime=prompt("Edit Your Time- (example-18:03)");
   const newValue = {
     todos: newTodo,
-    email: props.currentUser,
+    time: newTime
     
   };
     try {
@@ -88,15 +122,26 @@ const newTodo=prompt("Edit Your Todo");
         
           onChange={handleInputChange}
         />
+         <p>
+                <span style={{color:'grey'}}>Deadline At -example 12 which is 12pm : </span>
+             <input onChange={handleTimeChange} type="time" name="" id=""/>
+             </p>
         <button onClick={handleAddTodo}>Add Todo</button>
       </div>
+    
       <ul className="todo-list">
       {todoArray.map((todo, index) => (
           <li key={index}>
          <p>{todo.todos}</p>
+     
          <button className='update' onClick={() => handleUpdateTodo(todo.id)}>Update</button> 
             <button className='delete' onClick={() => handleDeleteTodo(todo.id)}>Delete</button>
+            <p>
+                
+            <p>Deadline at: {todo.time}</p>
+             </p>
           </li>
+           
         ))}
       </ul>
     </div>
